543603638344afa5384894b0f62d1d25f7736240
Venkatesh Appea <Venkatesh@pc98.tipm.ucalgary.ca>
2015-04-19 18:27:31 -0600

    Initial Commit

---
+++ Resistor Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Resistor Calculator
+//
+//  Created by Venkatesh Appea on 2015-04-19.
+//  Copyright (c) 2015 Venkatesh Appea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
